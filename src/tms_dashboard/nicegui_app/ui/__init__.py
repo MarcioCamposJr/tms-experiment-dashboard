41939e13767d10c06b6939edc751f27cff666d87
Marcio Campos <82606834+MarcioCamposJr@users.noreply.github.com>
2026-01-28 17:59:22 -0300

    Reapply "Merge branch 'master' of https://github.com/thaismarchetti/tms-experiment-dashboard into thaismarchetti-master"
    This reverts commit 3db02ae3f245ddcddf6f7c9cd8a93641110e8ed9.

--- src/tms_dashboard/nicegui_app/ui/__init__.py
+++ src/tms_dashboard/nicegui_app/ui/__init__.py
@@ -4,10 +4,19 @@
 
 from .header import create_header
 from .dashboard_tabs import create_dashboard_tabs
+<<<<<<< HEAD:src/tms_dashboard/nicegui_app/ui/__init__.py
 from .navigation_3d import create_3d_scene_with_models
+=======
+from .navigation_3d import create_navigation_3d, create_3d_scene_with_models
+>>>>>>> parent of 3db02ae (Revert "Merge branch 'master' of https://github.com/thaismarchetti/tms-experiment-dashboard into thaismarchetti-master"):src/tms_dashboard/nicegui_app/components/__init__.py
 from .checklist_tab import create_checklist_tab
 
 __all__ = [
     'create_header',
     'create_dashboard_tabs',
+<<<<<<< HEAD:src/tms_dashboard/nicegui_app/ui/__init__.py
+=======
+    'create_3d_scene_with_models',
+    'create_checklist_tab',
+>>>>>>> parent of 3db02ae (Revert "Merge branch 'master' of https://github.com/thaismarchetti/tms-experiment-dashboard into thaismarchetti-master"):src/tms_dashboard/nicegui_app/components/__init__.py
 ]
